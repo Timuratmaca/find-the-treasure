545d421a2342882501c0a01d7bbc8f35c6c30771
Timuratmaca <115021588+Timuratmaca@users.noreply.github.com>
2023-10-09 19:23:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,17 +2,32 @@ let good= document.getElementById("good")
 let map= document.getElementById("map")
 let button= document.getElementById("button")
 let pirate= document.getElementById("pirate")
+let timur= "HOT"
+let treasureX=Math.random()*500
+let treasureY=Math.random()*500
+
 
 
 
 
 map.onclick=function (event) {
-    pirate.style.left=event.offsetX-25+"px"
-    pirate.style.top=event.offsetY-50+"px"
+    let pirateX=event.offsetX-25
+    let pirateY=event.offsetY-50
+    pirate.style.left=pirateX+"px"
+    pirate.style.top=pirateY+"px"
+    let X=treasureX-pirateX
+    let Y=treasureY-pirateY
+    let dist=Math.sqrt(X**2+Y**2)
     // event.log(input.value)a
     // event.perevntdefaoult()
-    console.log("hellow world")
-    good.innerHTML=("HOT!!!!!!")
+    console.log(dist)
+    good.innerHTML=timur
+    if (dist<10) {
+        good.innerHTML= "you win!!!!!!!!!!!!!"
+    }
+    else if(dist<30){
+        good.innerHTML="boiling lava"
+    }
 }
 
 button.onclick=function (event){
