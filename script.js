4ec547f498513a2e7e1fea63752821424b583bb2
Timuratmaca <115021588+Timuratmaca@users.noreply.github.com>
2023-10-11 21:53:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let good= document.getElementById("good")
 let map= document.getElementById("map")
 let button= document.getElementById("button")
 let pirate= document.getElementById("pirate")
-let timur= "HOT"
+let timur= "ANTARTİCA"
 let treasureX=Math.random()*500
 let treasureY=Math.random()*500
 
@@ -23,11 +23,33 @@ map.onclick=function (event) {
     console.log(dist)
     good.innerHTML=timur
     if (dist<10) {
-        good.innerHTML= "you win!!!!!!!!!!!!!"
+        good.innerHTML= "YOU WİN!!!!!!!!!!!!!"
     }
-    else if(dist<30){
-        good.innerHTML="boiling lava"
+    else if(dist<40){
+        good.innerHTML="BOİLİNG LAVA!!!!!!!!!!!!!!"
     }
+    else if(dist<60){
+        good.innerHTML="VERY HOT!!!!"
+    }
+    else if(dist<80){
+        good.innerHTML="ooh yes thats not cold"
+    }
+    else if (dist<110){
+        good.innerHTML= "not so cold !!!"
+    }
+    else if(dist<160){
+        good.innerHTML=" yes NOT SO COLD "
+    }
+    else if(dist<200){
+        good.innerHTML= " not so cold ore its not???"
+    }
+    else if(dist<300){
+        good.innerHTML="COLD"
+    }
+    else if (dist<400){
+        good.innerHTML="NOT antaratica but so COLD"
+    }
+
 }
 
 button.onclick=function (event){
